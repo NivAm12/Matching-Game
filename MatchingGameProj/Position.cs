65d24a7ea590fc747921a13a99699942ab8b52d3
NivAm12 <nivamos12@gmail.com>
2020-07-14 18:10:57 +0300

    Upload the files

---
+++ MatchingGameProj/Position.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace B20_Ex02
+{
+    public class Position
+    {
+        // we dont want that the default position will be (0, 0), because its a legal position in our board.
+        private int m_Row = -1;
+        private int m_Col = -1;
+
+        public int Row
+        {
+            get
+            {
+                return m_Row;
+            }
+            set
+            {
+                m_Row = value;
+            }
+        }
+
+        public int Col
+        {
+            get
+            {
+                return m_Col;
+            }
+            set
+            {
+                m_Col = value;
+            }
+        }
+
+        public void SetPosition(int i_Row, int i_Col)
+        {
+            m_Row = i_Row;
+            m_Col = i_Col;
+        }
+    }
+}
---
+++ MatchingGameProj/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace B20_Ex02
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            MatchingGameUi game = new MatchingGameUi();
+            game.PlayGame();
+        }
+    }
+}
